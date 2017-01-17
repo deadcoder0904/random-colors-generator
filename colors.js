6d73b2186c4b5fd5b14de35ca75c81ab67e14383
Akshay Kadam <akshaykadam0904@gmail.com>
2017-01-17 23:15:12 +0530

    Adding script on Tue Jan 17 23:15:12 IST 2017 :skin-tone-3:

--- colors.js
+++ colors.js
@@ -18,7 +18,7 @@ const calculate = (inc,min,max) => {
 
 const generate2DigitOpacity = () => {
 	const opacity1 = randomNumber(0.7,1).toFixed(2)
-	const opacity2 = randomNumber(0.4,0.6).toFixed(2)
+	const opacity2 = randomNumber(0.8,1).toFixed(2)
 	return [
 		opacity1,
 		opacity2
